adb97b3034828deae6bcc4b6c624949659a816b8
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2024-03-05 10:27:36 +0100

    damage indicator

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,20 +1,44 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] private float maxHealth = 10f;
     [SerializeField] private GameObject bloodDropsFX;
+    [SerializeField] private Slider healthBar;
+    [SerializeField] private Image damageIndicator;
+    [SerializeField] private float dmgSmoothTime = 1f;
 
+    //private Color dmgColor = new(0f, 0f, 0f, 1f);
     private float currentHealth;
+    private bool getDamage;
 
     private void Start()
     {
+        getDamage = false;
         currentHealth = maxHealth;
+        healthBar.maxValue = maxHealth;
+        healthBar.value = maxHealth;
+    }
+
+    private void Update()
+    {
+        if (getDamage)
+        {
+            damageIndicator.color = new Color(255, 255, 255, 1);
+        }
+        else damageIndicator.color = Color.Lerp(
+            damageIndicator.color,
+            new(255, 255, 255, 0),
+            dmgSmoothTime * Time.deltaTime);
+        getDamage = false;
     }
 
     public void TakeDamage(float damage)
     {
         currentHealth -= damage;
+        getDamage = true;
+        healthBar.value = currentHealth;
         Instantiate(bloodDropsFX, transform.position, transform.rotation);
 
         if (currentHealth <= 0)
@@ -26,5 +50,6 @@ public class PlayerHealth : MonoBehaviour
     private void MakeDead()
     {
         Destroy(gameObject);
+        damageIndicator.color = new(255, 255, 255, 1);
     }
 }
