a4846114d5c4c71ee5a0d421fd825ed60f85bb41
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2024-03-07 09:43:54 +0100

    enemy healthbar

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,30 +4,38 @@ public class CameraFollow : MonoBehaviour
 {
     [SerializeField] private Transform target;
     [SerializeField] private float delay = 1f;
+    [SerializeField] private float viewDistance = 14f;
 
-    private Vector3 offsetRight;
-    private Vector3 offsetLeft;
+    //private Vector3 offsetRight;
+    //private Vector3 offsetLeft;
+    private Vector3 offset;
+
     private float lowY;
-    private bool facingRight;
-    private float difX;
+    //private float difX;
+    bool facingRight = true;
 
     private void Start()
     {
         lowY = transform.position.y;
-        difX = transform.position.x - target.position.x;
+        //difX = transform.position.x - target.position.x;
 
-        offsetRight = transform.position - target.position;
-        offsetLeft = transform.position - target.position;
-        offsetLeft.x -= 2 * difX;
-        facingRight = true;
+        offset = transform.position - target.position;
+        offset.x -= viewDistance / 2;
+        //offsetRight = transform.position - target.position;
+        //offsetLeft = transform.position - target.position;
+
     }
 
     private void FixedUpdate()
     {
 
-        Vector3 targetPos = target.localScale.x > 0
-             ? target.position + offsetRight
-             : target.position + offsetLeft;
+        if (facingRight != target.localScale.x < 0)
+        {
+            facingRight = target.localScale.x < 0;
+            offset.x += facingRight ? -viewDistance : viewDistance;
+        }
+
+        Vector3 targetPos = target.position + offset;
 
         transform.position = Vector3.Lerp(
             transform.position,
--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -1,18 +1,25 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyHealth : MonoBehaviour
 {
     [SerializeField] private float maxHealth = 5f;
+    [SerializeField] private GameObject deathFX;
+    [SerializeField] private Slider healthBar;
     private float currentHealth;
 
     private void Start()
     {
         currentHealth = maxHealth;
+        healthBar.maxValue = maxHealth;
+        healthBar.value = maxHealth;
     }
 
     public void TakeDamage(float damage)
     {
+        healthBar.gameObject.SetActive(true);
         currentHealth -= damage;
+        healthBar.value = currentHealth;
         if (currentHealth <= 0)
         {
             MakeDeath();
@@ -22,5 +29,6 @@ public class EnemyHealth : MonoBehaviour
     private void MakeDeath()
     {
         Destroy(gameObject);
+        Instantiate(deathFX, transform.position, transform.rotation);
     }
 }
